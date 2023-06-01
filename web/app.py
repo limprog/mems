c67b0604410fc0208593b8b5f9491abd87df85df
Limprog <ilinivan1602@gmail.com>
2023-06-01 14:50:31 +0300

    start

---
+++ web/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, session, request,  redirect, url_for, flash
+import datetime
+import sqlite3
+import os
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
